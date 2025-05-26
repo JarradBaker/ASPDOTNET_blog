d47cfb5af34bec2858b1e8b2ab61b859f64db134
Jarrad <jarradbaker@outlook.com>
2025-05-26 13:23:28 +0100

    Fix: fixed nullability warnings

--- DOTNET_DIARIES/Models/Blogpost.cs
+++ DOTNET_DIARIES/Models/Blogpost.cs
@@ -6,9 +6,9 @@ namespace DOTNET_DIARIES.Models
     public class Blogpost
     {
         public int Id { get; set; }
-        public string Title { get; set ;} 
-        public string ImageUrl { get; set; }
-        public string Content { get; set; }
+        public string Title { get; set ;} = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
         public DateTime PostedDate { get; set; }
         public List<BlogpostTag> BlogpostTags { get; set; } = [];
     }
--- DOTNET_DIARIES/Models/Tag.cs
+++ DOTNET_DIARIES/Models/Tag.cs
@@ -3,7 +3,7 @@ namespace DOTNET_DIARIES.Models
     public class Tag
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public List<BlogpostTag> BlogpostTags { get; set; } = [];
     }
 }
