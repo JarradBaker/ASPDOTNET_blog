611f99aed59daf9b0dd91d1cc238a93abf767c53
Jarrad <jarradbaker@outlook.com>
2025-06-03 11:25:32 +0100

    Fix: Removed froala, I ain't paying  a year buddy. Implemented TinyMCE

---
+++ DOTNET_DIARIES/Migrations/20250531084139_AddUrlHandleAndShortDescription.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DOTNET_DIARIES.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUrlHandleAndShortDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ShortDescription",
+                table: "Blogposts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UrlHandle",
+                table: "Blogposts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Blogposts",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "ShortDescription", "UrlHandle" },
+                values: new object[] { "", "" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShortDescription",
+                table: "Blogposts");
+
+            migrationBuilder.DropColumn(
+                name: "UrlHandle",
+                table: "Blogposts");
+        }
+    }
+}
--- DOTNET_DIARIES/Models/Blogpost.cs
+++ DOTNET_DIARIES/Models/Blogpost.cs
@@ -10,6 +10,8 @@ namespace DOTNET_DIARIES.Models
         public string ImageUrl { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
         public DateTime PostedDate { get; set; }
+        public string ShortDescription { get; set; } = string.Empty;
+        public string UrlHandle { get; set; } = string.Empty;
         public List<BlogpostTag> BlogpostTags { get; set; } = new();
     }
 }
